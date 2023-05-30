0afde86338990fa027066a30f37cfa67ab13375a
Lucas Archangelo <lustreet@gmail.com>
2023-05-30 12:25:12 -0300

    fix: fixing luck value engine and mint nft

--- Truffle/contracts/Game.sol
+++ Truffle/contracts/Game.sol
@@ -13,12 +13,11 @@ contract Game is Ownable, ReentrancyGuard, VRFV2Consumer {
     struct Upgrades {
         uint256 tokenId;
         uint8 luckValue;
-        bool upgrade;
         address sender;
     }
 
-    mapping (uint256 => Upgrades) upgrades;
-    uint8 difficulty = 6;
+    mapping(uint256 => Upgrades) upgrades;
+    uint8 difficulty = 4;
 
     constructor(
         address _nftWeaponAddress,
@@ -29,15 +28,17 @@ contract Game is Ownable, ReentrancyGuard, VRFV2Consumer {
         gameAddress = IERC721Weapon(_nftWeaponAddress);
     }
 
-    function forgeUpgradeWeapon(bool _isUpgrade, uint256 _tokenId, uint8 _luckValue) external {
-        if(_isUpgrade) {
-            require(gameAddress.ownerOf(_tokenId) == msg.sender);
-        }
+    function forgeUpgradeWeapon(uint256 _tokenId, uint8 _luckValue) external payable {
+        require(gameAddress.ownerOf(_tokenId) == msg.sender);
+        require(_luckValue < difficulty);
 
         uint256 chainlinkRequest = requestRandomWords(1);
         upgrades[chainlinkRequest].tokenId = _tokenId;
         upgrades[chainlinkRequest].luckValue = _luckValue;
-        upgrades[chainlinkRequest].upgrade = _isUpgrade;        
+    }
+
+    function buyWeapon() external payable {
+        gameAddress.safeMint(msg.sender);
     }
 
     function fulfillRandomWords(
@@ -45,17 +46,10 @@ contract Game is Ownable, ReentrancyGuard, VRFV2Consumer {
         uint256[] memory _randomWords
     ) internal override {
         super.fulfillRandomWords(_requestId, _randomWords);
-        
-        if((_randomWords[0] % difficulty) == upgrades[_requestId].luckValue) {
-            if(upgrades[_requestId].upgrade) {
-                gameAddress.levelUp(upgrades[_requestId].tokenId);
-            } else {
-                 gameAddress.safeMint(upgrades[_requestId].sender);
-            }
+        if ((_randomWords[0] % difficulty) == upgrades[_requestId].luckValue) {
+            gameAddress.levelUp(upgrades[_requestId].tokenId);
         } else {
-            if(upgrades[_requestId].upgrade) {
-                gameAddress.burn(upgrades[_requestId].tokenId);
-            }
+            gameAddress.burn(upgrades[_requestId].tokenId);
         }
     }
 
