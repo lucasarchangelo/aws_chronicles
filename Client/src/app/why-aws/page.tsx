07b25c5810634e7ff74dccea2b4b469d5097fcae
thiagorochatr <thiagord2016@gmail.com>
2023-06-10 16:11:41 -0300

    blog pages and header link to LP

---
+++ Client/src/app/why-aws/page.tsx
@@ -0,0 +1,7 @@
+export default function WhyAWS() {
+  return (
+    <>
+      <p>Why AWS</p>
+    </>
+  );
+}
--- Client/src/components/Header/index.tsx
+++ Client/src/components/Header/index.tsx
@@ -13,8 +13,10 @@ export const Header = () => {
   return (
     <header className="w-full h-[80px] flex bg-gradient-to-b from-black from-90% to-transparent">
       <div className="p-5 w-1/2">
-        <h1 className="font-medium text-3xl">Arcane Warriors Saga</h1>
-        <p className="font-thin text-xl">The AWS Chronicles</p>
+        <Link href="/">
+          <h1 className="text-3xl">Arcane Warriors Saga</h1>
+          <p className="text-xl">The AWS Chronicles</p>
+        </Link>
       </div>
       <div className="w-1/2 flex items-center justify-center bg-[#99C7F4] text-neutral-800">
         <ul className="flex w-full font-thin items-center justify-evenly">
